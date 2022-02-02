edd83a86faebed72ebcf3a7f297adff334f56626
suhitasva <suhitasucheta@hotmail.com>
2022-02-02 16:24:46 -0500

    Created base file structure for shiny app

---
+++ Hotel_Bookings/global.R
@@ -0,0 +1,2 @@
+
+hotels_df = read.csv('hotel_bookings.csv')
---
+++ Hotel_Bookings/server.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(datasets)
+library(funModeling) 
+library(Hmisc)
+
+library(shiny)
+
+
+function(input, output) {
+}
