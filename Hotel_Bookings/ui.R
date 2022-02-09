1d312a2a003ec6c55b54ca3be36270ac0967fb19
suhitasva <suhitasucheta@hotmail.com>
2022-02-09 16:17:45 -0500

    Added csv files in data folder

--- Hotel_Bookings/ui.R
+++ Hotel_Bookings/ui.R
@@ -6,6 +6,23 @@ dashboardPage(
   skin = "purple",
   dashboardHeader(title='Hotel Bookings Analysis',
                   titleWidth = 300),
-  dashboardSidebar(color = "teal", width = 300),
+  dashboardSidebar(width = 300,
+                   sidebarMenu(
+                     menuItem("Cancelled/Non-Cancelled Bookings", 
+                              tabName = "cancel",
+                              icon = icon("building")),
+                     menuItem("Cancellations by Deposit Type",
+                              tabName = "widgets",
+                              icon = icon("building")),
+                     menuItem("Cancellations by Distribution Type",
+                              tabName = "widgets",
+                              icon = icon("building")),
+                     menuItem("Cancellations by Guest Type",
+                              tabName = "widgets",
+                              icon = icon("building")),
+                     menuItem("Monthly Cancellations and ADR",
+                              tabName = "widgets",
+                              icon = icon("building")))),
+  
   dashboardBody()
 )
