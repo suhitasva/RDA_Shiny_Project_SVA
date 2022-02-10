63b12f0cbb3a7db16d0a1c27e95ea7576a16ac0d
suhitasva <suhitasucheta@hotmail.com>
2022-02-10 10:42:11 -0500

    Code written for dashboard structure, menu items created and a plot output tested in dashboard body

--- Hotel_Bookings/ui.R
+++ Hotel_Bookings/ui.R
@@ -10,19 +10,33 @@ dashboardPage(
                    sidebarMenu(
                      menuItem("Cancelled/Non-Cancelled Bookings", 
                               tabName = "cancel",
-                              icon = icon("building")),
-                     menuItem("Cancellations by Deposit Type",
-                              tabName = "widgets",
-                              icon = icon("building")),
-                     menuItem("Cancellations by Distribution Type",
-                              tabName = "widgets",
-                              icon = icon("building")),
-                     menuItem("Cancellations by Guest Type",
-                              tabName = "widgets",
-                              icon = icon("building")),
+                              icon = icon("concierge-bell")),
+                     menuItem("Booking Deposit Type",
+                              tabName = "deposit",
+                              icon = icon("concierge-bell")),
+                     menuItem("Hotel Distribution Type",
+                              tabName = "distribution",
+                              icon = icon("concierge-bell"),
+                              menuSubItem("Total Bookings", 
+                                          tabName = "tot_book"),
+                              menuSubItem("Average ADR",
+                                          tabName = "avg_adr")),
+                     menuItem("Repeat/Non-Repeat Guests",
+                              tabName = "guest",
+                              icon = icon("concierge-bell"),
+                              menuSubItem("Total Bookings", 
+                                          tabName = "tot_book_guest"),
+                              menuSubItem("Weekday/Weekend Nights Bookings",
+                                          tabName = "week_day_end")),
                      menuItem("Monthly Cancellations and ADR",
-                              tabName = "widgets",
-                              icon = icon("building")))),
+                              tabName = "monthly_adr",
+                              icon = icon("concierge-bell")))),
   
-  dashboardBody()
-)
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "cancel",
+              h2("Cancelled and Non Cancelled Bookings ratio by Hotel Types"),
+              mainPanel((plotOutput("count")))),
+      tabItem(tabName = "deposit",
+              h2("Number of bookings for different deposit types"))
+)))
