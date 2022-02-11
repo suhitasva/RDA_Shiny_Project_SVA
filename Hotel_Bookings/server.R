64a8e10e603d27361f6cbeccb27fcdc928c087f7
suhitasva <suhitasucheta@hotmail.com>
2022-02-11 10:40:50 -0500

    For cancellations by hotel types plots, and data table shown

--- Hotel_Bookings/server.R
+++ Hotel_Bookings/server.R
@@ -8,4 +8,26 @@ library(shiny)
 
 
 function(input, output) {
-}
+
+  output$freq <- renderPlot({
+    
+    filtered_df <- cancel_df %>% filter(hotel == input$hotel_type)
+    
+    if (input$hotel_type == "Both"){
+      filtered_df <- cancel_df}
+
+    filtered_df %>%
+                  ggplot(. ,aes(x = hotel,
+                                y = percent_cancellation,
+                                label = percent_cancellation,
+                                fill = is_canceled)) +
+                  geom_col(stat = "identity", width = 0.5) +
+                  geom_text(size = 3.5, position = position_stack(vjust = 0.5),
+                            colour = "white", fontface = "bold") +
+                  labs(title ="Percent Cancellations by Hotel Type", x = "Type of Hotel",
+                       y = "Percentage of bookings (%)") +
+                  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank())})
+
+  output$canceldf <- renderTable({cancel_df}, striped = TRUE, hover=TRUE,
+                                 bordered=TRUE, spacing = "l", width="670px",
+                                 align="c")}
